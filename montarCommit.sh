cdc9dcecf77f8b1395c1a38b68fd69f4a3fcf56d
juniorots <juniorots@gmail.com>
2021-04-12 16:18:15 -0300

    Atualizando Projeto 0° BRAVO

---
+++ montarCommit.sh
@@ -0,0 +1,9 @@
+git add package.json
+git add package-lock.json
+git add montarCommit.sh
+git add documentation/*
+git add public/*
+git add routes/*
+git add app.js
+
+git commit -m "Atualizando Projeto 0° BRAVO"
